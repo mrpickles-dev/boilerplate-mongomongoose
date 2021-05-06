eaacd2b02ce72ecaff696376f9a6c20047eca18b
mrpickles-dev <mr.pickles300@gmail.com>
2021-05-06 20:28:56 +0200

    update

--- myApp.js
+++ myApp.js
@@ -1,23 +1,78 @@
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI);
 
-let Person;
+const Schema = mongoose.Schema;
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const createAndSavePerson = done => {
+  let marco = new Person({
+    name: "Marco",
+    age: 20,
+    favoriteFoods: ["Pizza"]
+  });
+
+  marco.save((error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      done(null, data);
+    }
+  });
 };
+
+let arrayOfPeople = [
+  {
+    name: "Gino",
+    age: 65,
+    favoriteFoods: ["pizza", "lemon", "apple"]
+  },
+  { name: "Andrea", age: 34, favoriteFoods: ["watermelon", "mango"] },
+  { name: "antonio", age: 22, favoriteFoods: ["sushi"] }
+];
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (error, createdPeople) => {
+    if (error) {
+      console.log(error);
+    } else {
+      done(null, createdPeople);
+    }
+  });
 };
 
+Person.find({ name: "Kris", age: 42 }, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (error, arrayOfResults) => {
+    if (error) {
+      console.log(error);
+    } else {
+      done(null, arrayOfResults);
+    }
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: { $all: [food] } }, (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      done(null, result);
+    }
+  });
 };
 
 const findPersonById = (personId, done) => {
@@ -40,17 +95,19 @@ const removeById = (personId, done) => {
   done(null /*, data*/);
 };
 
-const removeManyPeople = (done) => {
+const removeManyPeople = done => {
   const nameToRemove = "Mary";
 
   done(null /*, data*/);
 };
 
-const queryChain = (done) => {
+const queryChain = done => {
   const foodToSearch = "burrito";
 
   done(null /*, data*/);
 };
+
+console.log(process.env.MongoDB_uri);
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
